2bbb7dd56f84da5b2c59f84452fc40416a7cc4ec
Frank Villafuerte <133295276+Frank-Villafuerte@users.noreply.github.com>
2024-05-29 21:58:28 -0500

    definimos las vistas de los formularios en view.py

--- proyecto/gestion_notas/views.py
+++ proyecto/gestion_notas/views.py
@@ -1,4 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AlumnoForm, CursoForm, NotaForm
+from .models import Alumno, Curso, Nota
+
+def crear_alumno(request):
+    if request.method == 'POST':
+        form = AlumnoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_alumnos')
+    else:
+        form = AlumnoForm()
+    return render(request, 'gestion/crear_alumno.html', {'form': form})
+
+def lista_alumnos(request):
+    alumnos = Alumno.objects.all()
+    return render(request, 'gestion/lista_alumnos.html', {'alumnos': alumnos})
+
+def crear_curso(request):
+    if request.method == 'POST':
+        form = CursoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_cursos')
+    else:
+        form = CursoForm()
+    return render(request, 'gestion/crear_curso.html', {'form': form})
+
+def lista_cursos(request):
+    cursos = Curso.objects.all()
+    return render(request, 'gestion/lista_cursos.html', {'cursos': cursos})
+
+def crear_nota(request):
+    if request.method == 'POST':
+        form = NotaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_notas')
+    else:
+        form = NotaForm()
+    return render(request, 'gestion/crear_nota.html', {'form': form})
+
+def lista_notas(request):
+    notas = Nota.objects.all()
+    return render(request, 'gestion/lista_notas.html', {'notas': notas})
 
 
 
